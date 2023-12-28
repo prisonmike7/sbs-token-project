bd5c83c2f43214eb506e7edc04e92670e8fddf20
“Michael <mwharris7@gmail.com>
2023-12-27 23:17:20 -0700

    First Commit_1

---
+++ test/SonicbassToken.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {SonicbassToken} from "../src/SonicbassToken.sol";
+
+contract CounterTest is Test {
+    
+    SonicbassToken public counter;
+
+}
